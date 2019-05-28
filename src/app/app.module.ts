345eab159863e98712210a4c0ec0c647b968fcc7
Bartłomiej Więckowski <bartlomiej.wieckowski@syncron.com>
2019-05-28 16:05:45 +0200

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PostComponent } from './post/post.component';
+import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
@@ -14,7 +15,10 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'posts', component: PostComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
